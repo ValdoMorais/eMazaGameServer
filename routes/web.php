20f753ea3c4f810315da8def16390f15d369821c
Valdo Flor <82148347+ValdoMoraisFlor@users.noreply.github.com>
2022-10-11 11:59:50 +0200

    chore: integracao de voyager

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use App\Http\Controllers\GenericController;
 
 
 Route::get('/', [GenericController::class, 'lending']);
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
